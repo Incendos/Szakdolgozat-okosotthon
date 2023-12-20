69800eaa14bbe7166d13273e9367f9b7d3fb764e
Dimand Márton <dimand.marton@gmail.com>
2023-12-20 15:44:51 +0100

    Added more sample codes

--- src/main/java/hu/unideb/inf/Main.java
+++ src/main/java/hu/unideb/inf/Main.java
@@ -3,6 +3,12 @@ package hu.unideb.inf;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 import hu.unideb.inf.controllers.smartsocket.MqttSocketController;
 import hu.unideb.inf.controllers.smartsocket.SmartSocketController;
+import hu.unideb.inf.sensor.contact.ContactSensor;
+import hu.unideb.inf.sensor.contact.MqttContactSensor;
+import hu.unideb.inf.sensor.motion.MotionSensor;
+import hu.unideb.inf.sensor.motion.MqttMotionSensor;
+import hu.unideb.inf.sensor.temperature.MqttTemperatureSensor;
+import hu.unideb.inf.sensor.temperature.TemperatureSensor;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,11 +27,25 @@ public class Main {
 
         SmartSocketController TasmotaController = MqttSocketController.builder()
                 .defaultTasmotaConfig("tasmota_068573")
-                .mqttClient(mqtt5Client)
-                .build();
+                .mqttClient(mqtt5Client).build();
         TasmotaController.initConnection();
 
-        TuyaController.toggle();
-        TasmotaController.toggle();
+        ContactSensor SonoffContactSensor = MqttContactSensor.builder()
+                .defaultSonoffConfig("DoorSensor001")
+                .mqttClient(mqtt5Client)
+                .build();
+        SonoffContactSensor.initConnection();
+
+        MotionSensor SonoffMotionSensor = MqttMotionSensor.builder()
+                .defaultSonoffConfig("MotionSensor001")
+                .mqttClient(mqtt5Client)
+                .build();
+        SonoffMotionSensor.initConnection();
+
+        TemperatureSensor TasmotaTempSensor = MqttTemperatureSensor.builder()
+                .defaultTasmotaConfig("tasmota_99562A")
+                .mqttClient(mqtt5Client)
+                .build();
+        TasmotaTempSensor.initConnection();
     }
 }

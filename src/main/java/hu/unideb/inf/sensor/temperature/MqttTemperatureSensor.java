5e65919eeaa27d7ccac2cc28eeec2c9afe6aba36
Dimand Márton <dimand.marton@gmail.com>
2023-12-21 15:31:00 +0100

    Added class documentation to TemperatureSensor classes

--- src/main/java/hu/unideb/inf/sensor/temperature/MqttTemperatureSensor.java
+++ src/main/java/hu/unideb/inf/sensor/temperature/MqttTemperatureSensor.java
@@ -12,17 +12,35 @@ import java.util.function.Function;
 
 @Builder
 public class MqttTemperatureSensor implements TemperatureSensor {
+
     private final ReadOnlyDoubleWrapper temperatureWrapper = new ReadOnlyDoubleWrapper(0.0);
 
+    /**
+     * The mqtt client that is used to publish messages and subscribe to topics.
+     */
     private Mqtt5Client mqttClient;
 
+    /**
+     * The friendly name of the device we wish to control.
+     */
     private String friendlyName;
 
+    /**
+     * The topic that the controlled device publishes its state to.
+     * Each list element corresponds to the next topic level in order. <br>
+     * ["zigbee2mqtt", "friendlyName", "Power"] would indicate the following topic: "zigbee2mqtt/friendlyName/Power"
+     */
     @Singular("temperatureTopic")
     private List<String> temperatureTopic;
 
+    /**
+     * A mapper function that transforms the payload we get on the {@link #temperatureTopic} into a Double value.
+     */
     private Function<String, Double> temperatureQuery;
 
+    /**
+     * Connects to the broker if not already connected and subscribes to the {@link #temperatureTopic}.
+     */
     @Override
     public void initConnection() {
         if(mqttClient.getConfig().getConnectionConfig().isEmpty()) {
@@ -44,7 +62,16 @@ public class MqttTemperatureSensor implements TemperatureSensor {
     }
 
     public static class MqttTemperatureSensorBuilder {
+        /**
+         * For more information: <a href="https://tasmota.github.io/docs/MQTT/">https://tasmota.github.io/docs/MQTT/</a>
+         * @return the builder configured for a Tasmota device.
+         */
         public MqttTemperatureSensorBuilder defaultTasmotaConfig(String friendlyName) {
+            /**
+             * Tasmota devices usually publish every telemetry data under 1 topic in json format,
+             * While other devices can be configured to publish each data on a different topics, this is not true for Tasmota.
+             * @TODO switch to a json converter and/or create new class for Tasmota devices?
+             */
             return this.friendlyName(friendlyName)
                     .temperatureTopic("tele").temperatureTopic(friendlyName).temperatureTopic("SENSOR")
                     .temperatureQuery((response) -> {
--- src/main/java/hu/unideb/inf/sensor/temperature/TemperatureSensor.java
+++ src/main/java/hu/unideb/inf/sensor/temperature/TemperatureSensor.java
@@ -1,10 +1,15 @@
 package hu.unideb.inf.sensor.temperature;
 
-import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 
 public interface TemperatureSensor {
+    /**
+     * @return the last measured temperature (in Celsius) wrapped in a Double Property.
+     */
     ReadOnlyDoubleProperty getTemperatureProperty();
 
+    /**
+     * Initializes connection to the smart device. This should be called right after instancing a class.
+     */
     void initConnection();
 }

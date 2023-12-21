06996caaf81b05272ef1a76eb98bc463c75e28cd
Dimand Márton <dimand.marton@gmail.com>
2023-12-21 15:01:50 +0100

    Added class documentation to socket classes

--- src/main/java/hu/unideb/inf/controllers/smartsocket/MqttSocketController.java
+++ src/main/java/hu/unideb/inf/controllers/smartsocket/MqttSocketController.java
@@ -2,6 +2,8 @@ package hu.unideb.inf.controllers.smartsocket;
 
 
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.ReadOnlyBooleanWrapper;
 import lombok.Builder;
 import lombok.Singular;
 
@@ -12,26 +14,70 @@ import java.util.function.Function;
 @Builder
 public class MqttSocketController implements SmartSocketController {
 
+    /**
+     * The mqtt client that is used to publish messages and subscribe to topics.
+     */
     private Mqtt5Client mqttClient;
 
+    /**
+     * The friendly name of the device we wish to control.
+     */
     private String friendlyName;
 
+    /**
+     * The power state of the socket.
+     */
+    private final ReadOnlyBooleanWrapper isPowerOn = new ReadOnlyBooleanWrapper(false);
+
+    /**
+     * The topic the controlled device subscribes to in order to listen for power commands.
+     * Each list element corresponds to the next topic level in order. <br>
+     * ["zigbee2mqtt", "friendlyName", "Power"] would indicate the following topic: "zigbee2mqtt/friendlyName/Power"
+     */
     @Singular("powerCommandTopic")
     private List<String> powerCommandTopic;
 
+    /**
+     * The topic that the controlled device publishes its state to.
+     * Each list element corresponds to the next topic level in order. <br>
+     * ["zigbee2mqtt", "friendlyName", "Power"] would indicate the following topic: "zigbee2mqtt/friendlyName/Power"
+     */
     @Singular("powerStateTopic")
     private List<String> powerStateTopic;
 
+    /**
+     * The topic we can publish to in order to make the device publish its power state.
+     * Each list element corresponds to the next topic level in order. <br>
+     * ["zigbee2mqtt", "friendlyName", "Power"] would indicate the following topic: "zigbee2mqtt/friendlyName/Power"
+     */
     @Singular("powerQueryTopic")
     private List<String> powerQueryTopic;
 
+    /**
+     * The payload we send on the {@link #powerCommandTopic} to toggle power.
+     */
     private String powerToggleParameter;
+    /**
+     * The payload we send on the {@link #powerCommandTopic} to turn on power.
+     */
     private String powerOnParameter;
+    /**
+     * The payload we send on the {@link #powerCommandTopic} to turn off power.
+     */
     private String powerOffParameter;
+    /**
+     * The payload we send on the {@link #powerQueryTopic}.
+     */
     private String powerQueryParameter;
 
+    /**
+     * A mapper function that transforms the payload we get on the {@link #powerStateTopic} into a boolean.
+     */
     private Function<String, Boolean> powerStateQuery;
 
+    /**
+     * Connects to the broker if not already connected and subscribes to the {@link #powerStateTopic}
+     */
     @Override
     public void initConnection() {
         if(mqttClient.getConfig().getConnectionConfig().isEmpty()) {
@@ -47,6 +93,10 @@ public class MqttSocketController implements SmartSocketController {
                 .send();
     }
 
+    /**
+     * publishes the {@link #powerToggleParameter} to the {@link #powerCommandTopic} in order to
+     * toggle the power on the device.
+     */
     @Override
     public void toggle() {
         mqttClient.toBlocking().publishWith()
@@ -55,6 +105,12 @@ public class MqttSocketController implements SmartSocketController {
                 .send();
     }
 
+    /**
+     * publishes to the {@link #powerCommandTopic} in order to
+     * turn on/off the power on the device.
+     * @param isOn the desired state we want to set. if true sends the {@link #powerOnParameter} as payload, otherwise
+     *             sends {@link #powerOffParameter}
+     */
     @Override
     public void setPower(boolean isOn) {
         mqttClient.toBlocking().publishWith()
@@ -63,7 +119,16 @@ public class MqttSocketController implements SmartSocketController {
                 .send();
     }
 
+    @Override
+    public ReadOnlyBooleanProperty getIsPowerOnProperty() {
+        return isPowerOn.getReadOnlyProperty();
+    }
+
     public static class MqttSocketControllerBuilder {
+        /**
+         * For more information: <a href="https://tasmota.github.io/docs/MQTT/">https://tasmota.github.io/docs/MQTT/</a>
+         * @return the builder configured for a Tasmota device.
+         */
         public MqttSocketControllerBuilder defaultTasmotaConfig(String friendlyName) {
             return this.friendlyName(friendlyName)
                     .powerToggleParameter("Toggle")
@@ -78,6 +143,10 @@ public class MqttSocketController implements SmartSocketController {
                     });
         }
 
+        /**
+         * For more information: <a href="https://www.zigbee2mqtt.io/devices/TS011F_plug_1.html">https://www.zigbee2mqtt.io/devices/TS011F_plug_1.html</a>
+         * @return the builder configured for a Tuya device.
+         */
         public MqttSocketControllerBuilder defaultTuyaConfig(String friendlyName) {
             return this.friendlyName(friendlyName)
                     .powerToggleParameter("{\"state\": \"TOGGLE\"}")
--- src/main/java/hu/unideb/inf/controllers/smartsocket/SmartSocketController.java
+++ src/main/java/hu/unideb/inf/controllers/smartsocket/SmartSocketController.java
@@ -1,10 +1,27 @@
 package hu.unideb.inf.controllers.smartsocket;
 
-import javafx.beans.property.ReadOnlyBooleanWrapper;
+import javafx.beans.property.ReadOnlyBooleanProperty;
 
 public interface SmartSocketController {
-    ReadOnlyBooleanWrapper isPowerOn = new ReadOnlyBooleanWrapper(false);
+    /**
+     * If the state of the device changes it will be reflected through the returned value.
+     * @return a wrapped boolean that indicates if the socket is on or off.
+     */
+    ReadOnlyBooleanProperty getIsPowerOnProperty();
+
+    /**
+     * Initializes connection to the smart device. This should be called right after instancing a class.
+     */
     void initConnection();
+
+    /**
+     * Inverts the state of the device. (If it's on, then turns it off)
+     */
     void toggle();
+
+    /**
+     * Sets the state of the device.
+     * @param isOn the desired state.
+     */
     void setPower(boolean isOn);
 }
